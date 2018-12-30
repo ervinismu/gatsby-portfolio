5172a096054a8139739f73c39217454644fab85d
ervinismu <social.ervin@gmail.com>
2018-12-30 22:28:02 +0700

    Cahange mail

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -272,7 +272,7 @@ const Index = () => (
         <Inner>
           <Title>Get in touch</Title>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
+            Say <a href="mailto:social.ervin@gmail.com">Hi</a> or find me on other platforms:{' '}
             <a href="https://www.instagram.com/ervinismu">Instagram</a>
           </ContactText>
         </Inner>
